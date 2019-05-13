9452fbd0e0ab9891357746432ad5f491d6c40f22
naskakov <naska.kov05@gmail.com>
2019-05-14 02:34:48 +0300

    indicator table

--- ui_components/update_components.sh
+++ ui_components/update_components.sh
@@ -62,6 +62,7 @@ cp -Rfv $base_path/components/salt/static/* $sc_web_static_path
 cp -Rfv $base_path/components/wavelength/static/* $sc_web_static_path
 cp -Rfv $base_path/components/solubility_table/static/* $sc_web_static_path
 cp -Rfv $base_path/components/drFormula/static/* $sc_web_static_path
+cp -Rfv $base_path/components/indicator_table/static/* $sc_web_static_path
 
 stage "Install component"
 
@@ -76,9 +77,11 @@ append_js $sc_web_path/templates/components.html components/js/chemistry_compone
 append_js $sc_web_path/templates/components.html components/js/chemistry_components/wavelength_ui_component.js
 append_js $sc_web_path/templates/components.html components/js/chemistry_components/solubility_table.js
 append_js $sc_web_path/templates/components.html components/js/chemistry_components/drFormula.js
+append_js $sc_web_path/templates/components.html components/js/chemistry_components/indicator_table.js
 
 append_css $sc_web_path/templates/components.html components/css/chemistry_components/solubility_table.css
 append_css $sc_web_path/templates/components.html components/css/chemistry_components/ui_periodic_table.css
+append_css $sc_web_path/templates/components.html components/css/chemistry_components/ui_indicator_table.css
 
 cd ../../sc-web/scripts
 ./install_deps_ubuntu.sh
